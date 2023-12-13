388522f4fb7647e8d8665d999f318f14895d53ae
Ibnu Huygenz <ibnuwdodo@gmail.com>
2023-12-13 17:03:07 +0700

    add procfile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
