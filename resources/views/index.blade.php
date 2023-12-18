015f81a9b3167770f7d585d4a9adf0539bc4d3aa
Ibnu Huygenz <ibnuwdodo@gmail.com>
2023-12-18 18:50:54 +0700

    refactor: page become dashboard (see last user), show-total (see total user with cheaper read in db), all-members (see all members and total user, most expensive but still cheaper in db)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,64 @@
+<x-content>
+    <x-slot name="title">
+        {{ __('Member BSO Dimensi Web') }}
+    </x-slot>
+    <div class="row p-4">
+        <div class="col-12 mb-4">
+            <table class="table">
+                <thead>
+                <tr class="text-center">
+                    <th scope="col" class="w-20">
+                        No
+                    </th>
+                    <th scope="col" class="text-capitalize">
+                        No Whatsapp
+                    </th>
+                    <th scope="col" class="text-capitalize">
+                        Nama
+                    </th>
+                    <th scope="col">
+                        NIM
+                    </th>
+                    <th scope="col">
+                        Harapan
+                    </th>
+                    <th scope="col">
+                        Bidang
+                    </th>
+                    <th scope="col">
+                        Waktu Pendaftaran
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($datas as $data)
+                    <tr>
+                        <td class="text-center">
+                            {{ $loop->iteration }}
+                        </td>
+                        <td>
+                            {{ $data->nowa }}
+                        </td>
+                        <td>
+                            {{ $data->nama }}
+                        </td>
+                        <td>
+                            {{ $data->nim }}
+                        </td>
+                        <td>
+                            {{ $data->harapan }}
+                        </td>
+                        <td>
+                            {{ $data->bidang }}
+                        </td>
+                        <td>
+                            {{ $data->waktu_pendaftaran }}
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-content>
+
---
+++ resources/views/total.blade.php
@@ -0,0 +1,6 @@
+<x-content>
+    <x-slot name="title">
+        {{ __('Total Member BSO Dimensi Web') }} ({{ $total }} Pendaftar)
+    </x-slot>
+</x-content>
+
